b114146492b496c4a4f11d4646c8a6b8af24de0a
Leifur Halldór Ásgeirsson <leifurhauks@gmail.com>
2023-03-28 20:07:33 -0400

    fix version identifier

--- bookwyrm/settings.py
+++ bookwyrm/settings.py
@@ -11,7 +11,7 @@ from django.utils.translation import gettext_lazy as _
 env = Env()
 env.read_env()
 DOMAIN = env("DOMAIN")
-VERSION = "0.5.5-millefeuilles"
+VERSION = "0.5.5+millefeuilles"
 
 RELEASE_API = env(
     "RELEASE_API",
